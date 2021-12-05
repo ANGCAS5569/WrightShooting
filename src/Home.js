898c18cc7651ccbd0c0b7083c586356819cf8c3c
ANGCAS5569 <angcas5569@students.ecpi.edu>
2021-12-04 23:37:10 -0600

    added banner

--- src/Home.js
+++ src/Home.js
@@ -4,9 +4,12 @@ class Home extends Component {
   render() {
     return (
       <div className = "Home">
-        <h2>HELLO</h2>
-        <p>This page is in progress.</p>
- 
+        <div className = "Banner"> 
+          <h3 className = "BannerContent1" >Our Mission:</h3>
+          <p className = "BannerContent2">We will equip our law enforcement, military, and security forces brothers and 
+            sisters with comprehensive training and support that bridges the gap between 
+            basic level shooting skills to advanced techniques. </p>
+        </div> 
       </div>
     );
   }
