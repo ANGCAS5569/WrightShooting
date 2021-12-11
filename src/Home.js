ddc359ef15cf3e07601db05d4bba5831b3a70804
ANGCAS5569 <angcas5569@students.ecpi.edu>
2021-12-11 17:44:18 -0600

    with review box started

--- src/Home.js
+++ src/Home.js
@@ -21,11 +21,21 @@ class Home extends Component {
         </div>
         <div className = "training">
           <img button src={Image} alt="training picture" width="300"></img>
-          <p ClassName= "training-info">TRAINING<br></br>CLASS INFO</p>
+          <p className= "training-info">TRAINING<br></br>CLASS INFO</p>
         </div>
         <div className = "targets">
           <img button src={Image2} alt="targets picture" width="300" height="198"></img>
-          <p ClassName= "targets-info">TARGETS<br></br>SHOP</p>
+          <p className= "targets-info">TARGETS<br></br>SHOP</p>
+        </div>
+        <div className ="classReviews">
+        <h4>Check out some of our Class<br></br>Reviews!</h4>
+        <p className="testimonials">This class was extremely informative and allowed for <br></br>
+          the students to identify their weakness. The class <br></br>
+          also gave students new training scenarios and techniques <br></br>
+          to apply while teaching. During the class Brandon included <br></br>
+          all of the students while he explained a course and he was <br></br>
+           very humble and didn't make his students feel belittled <br></br>
+           because of his extensive training. Overall great class</p>
         </div>
       </div>
     );
