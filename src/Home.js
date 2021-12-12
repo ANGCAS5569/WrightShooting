468d64c89b5bbbf751a1c1941233c7d6221901eb
ANGCAS5569 <angcas5569@students.ecpi.edu>
2021-12-11 19:59:38 -0600

    modified buttons

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Image from './training.jpg';
 import Image2 from './targets.JPG';
+
  
 class Home extends Component {
   render() {
@@ -20,22 +21,52 @@ class Home extends Component {
           <button className="button1">Subscribe Now</button>
         </div>
         <div className = "training">
-          <img button src={Image} alt="training picture" width="300"></img>
-          <p className= "training-info">TRAINING<br></br>CLASS INFO</p>
+        <a href="/Firearms">
+            <img button src={Image} alt="training picture" width="300"></img>
+            <p className= "training-info">TRAINING<br></br>CLASS INFO</p></a>
         </div>
         <div className = "targets">
-          <img button src={Image2} alt="targets picture" width="300" height="198"></img>
-          <p className= "targets-info">TARGETS<br></br>SHOP</p>
+          <a href="/Shop">
+            <img button src={Image2} alt="targets picture" width="300" height="198"></img>
+            <p className= "targets-info">TARGETS<br></br>SHOP</p></a>
         </div>
         <div className ="classReviews">
         <h4>Check out some of our Class<br></br>Reviews!</h4>
-        <p className="testimonials">This class was extremely informative and allowed for <br></br>
-          the students to identify their weakness. The class <br></br>
-          also gave students new training scenarios and techniques <br></br>
-          to apply while teaching. During the class Brandon included <br></br>
-          all of the students while he explained a course and he was <br></br>
-           very humble and didn't make his students feel belittled <br></br>
-           because of his extensive training. Overall great class</p>
+        <div className="testimonials">
+          <p >This class was extremely informative and allowed for <br></br>
+            the students to identify their weakness. The class <br></br>
+            also gave students new training scenarios and techniques <br></br>
+            to apply while teaching. During the class Brandon included <br></br>
+            all of the students while he explained a course and he was <br></br>
+            very humble and didn't make his students feel belittled <br></br>
+            because of his extensive training. Overall great class</p>
+          <p>Domonic Zammit   Dec 8, 2021</p>
+          <br></br>
+          <p >This class was extremely informative and allowed for <br></br>
+            the students to identify their weakness. The class <br></br>
+            also gave students new training scenarios and techniques <br></br>
+            to apply while teaching. During the class Brandon included <br></br>
+            all of the students while he explained a course and he was <br></br>
+            very humble and didn't make his students feel belittled <br></br>
+            because of his extensive training. Overall great class</p>
+          <p>Domonic Zammit   Dec 8, 2021</p>
+          <br></br>
+          <p >Brandon's Pistol Optics class is well worth your time to <br></br>
+            attend. It's a detailed progression to transition an iron <br></br>
+            sights handgun shooter to a dot with twenty hours of work. <br></br>
+            It's simple enough for a basic shooter, but will challenge <br></br>
+            more advanced shooters to get faster and more accurate. His <br></br>
+            class will be very influential on our red dot handgun program.</p>
+          <p>D/S David Lane   Nov 24, 2021</p>
+          <br></br>
+          <p >Brandon's Pistol Optics class is well worth your time to <br></br>
+            attend. It's a detailed progression to transition an iron <br></br>
+            sights handgun shooter to a dot with twenty hours of work. <br></br>
+            It's simple enough for a basic shooter, but will challenge <br></br>
+            more advanced shooters to get faster and more accurate. His <br></br>
+            class will be very influential on our red dot handgun program.</p>
+          <p>D/S David Lane   Nov 24, 2021</p>
+          </div>
         </div>
       </div>
     );
