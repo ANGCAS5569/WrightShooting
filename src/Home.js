8959277520b837807d4e98562f0b6f256b6af91e
ANGCAS5569 <angcas5569@students.ecpi.edu>
2021-12-11 20:50:33 -0600

    Rough Draft complete

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import Image from './training.jpg';
 import Image2 from './targets.JPG';
+import Image3 from './gun.jpg';
+import Image4 from './shirt.jpg';
+import Image5 from './holo.jpg';
 
  
 class Home extends Component {
@@ -67,6 +70,41 @@ class Home extends Component {
             class will be very influential on our red dot handgun program.</p>
           <p>D/S David Lane   Nov 24, 2021</p>
           </div>
+        </div>
+        <div className = "topPicks"><h2>TOP PICKS:</h2></div>
+        <div className="FN">
+          <a href="/Shop">
+              <img button src={Image3} alt="FN" width="300" height="198"></img>
+              <p className= "FN-info">TacRack - FN 509 Series</p>
+                <div className="flex"><p className="discount">$39.99</p><p>$29.99</p></div>
+              </a>
+        </div>
+        <div className="shirt">
+          <a href="/Shop">
+              <img button src={Image4} alt="shirt" width="300" height="198"></img>
+              <p className= "shirt-info">WrightShooting T-Shirt</p>
+                <p>$24.99</p>
+              </a>
+        </div>
+        <div className="holo">
+          <a href="/Shop">
+              <img button src={Image5} alt="holo" width="300" height="198"></img>
+              <p className= "holo-info">Holosun Technologies, 
+              407K-X2,
+              <br></br> Red Dot, 6 MOA Dot, Black,
+              <br></br> Side Battery</p>
+                <p>$205</p>
+              </a>
+        </div>
+        <div className="footer">
+          <h3>WrightShooting
+            <br></br>
+            52 Tuscan Way Suite 202 #131
+            <br></br>
+            St. Augustine, FL 32092
+            <br></br>
+            admin@wrightshooting.com
+          </h3>
         </div>
       </div>
     );
